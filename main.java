7e03c0b3101de432870b94aefe5d1e94835476fb
Gedigarov <100475695+Gedigarov@users.noreply.github.com>
2022-05-29 13:59:33 +0300

    Задание два

--- main.java
+++ main.java
@@ -3,5 +3,9 @@ package com.max.idea;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        double tkt = 5.234234;
+        byte tit = 127;
+        System.out.println(tkt);
+        System.out.println(tit);
     }
 }
