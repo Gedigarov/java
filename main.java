71fda6473153bae4628b780fd64adcdaf989650e
Gedigarov <100475695+Gedigarov@users.noreply.github.com>
2022-05-30 22:41:47 +0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,11 +1,14 @@
 package com.max.idea;
 
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        double tkt = 5.234234;
-        byte tit = 127;
-        System.out.println(tkt);
-        System.out.println(tit);
+        public static void main(String[] args) {
+            int[] nums = new int[5];
+            nums[4] = 5;
+            nums[1] = 2;
+            nums[2] = 3;
+            nums[3] = 4;
+            nums[0] = 1;
+            int sum = nums[nums.length - 1] + nums[nums.length - 3];
+            System.out.println(sum);
+        }
     }
-}
