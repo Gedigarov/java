c20c19076c88fb3e5567d71059715c30ffb4967a
Gedigarov <100475695+Gedigarov@users.noreply.github.com>
2022-05-31 18:46:09 +0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,14 +1,20 @@
 package com.max.idea;
 
 public class main {
-        public static void main(String[] args) {
-            int[] nums = new int[5];
-            nums[4] = 5;
-            nums[1] = 2;
-            nums[2] = 3;
-            nums[3] = 4;
-            nums[0] = 1;
-            int sum = nums[nums.length - 1] + nums[nums.length - 3];
-            System.out.println(sum);
-        }
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 5;
+
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+
+        int sum = nums[nums.length - 3] + nums[nums.length - 5];
+        System.out.println(sum);
+
     }
+}
