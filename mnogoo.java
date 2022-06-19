43ca2197ea09b63d37f801272f2a93a3591ad7bb
Gedigarov <100475695+Gedigarov@users.noreply.github.com>
2022-06-19 18:47:49 +0300

    Add files via upload

---
+++ mnogoo.java
@@ -0,0 +1,21 @@
+package four.five;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class mnogoo {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите слова ");
+        String line = scan.nextLine();
+        Pattern pat = Pattern.compile("\\b[A-Za-z]+\\b");
+        Matcher mat = pat.matcher(line);
+        int count = 0;
+        while (mat.find()) {
+            count++;
+            System.out.print(mat.group() + " ");
+        }
+        System.out.println( "\nКоличество слов кириллицей: "+ count);
+    }
+}
